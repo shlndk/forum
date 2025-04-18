0cff2e7b890b274dfba67a1af39b6d9edf3ff60f
shlendyk <shlendikgghggg@gmail.com>
2025-04-18 20:25:44 +0300

    Промежуточный коммит перед переключением ветки

--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -10,24 +10,28 @@
 @section('content')
     @include('layouts.other.nav')
     <div class="container mt-5">
-        <h2>Post search</h2>
+        <h2>Поиск постов</h2>
         <form action="{{ route('searchForm') }}" method="GET" class="mb-4">
             <div class="input-group">
-                <input type="text" name="search" class="form-control" placeholder="Enter a subject title..."
+                <input type="text" name="search" class="form-control" placeholder="Введите название темы..."
                        value="{{ request('search') }}" required>
-                <button class="btn btn-secondary" type="submit">Search</button>
+                <button class="btn btn-secondary" type="submit">Поиск</button>
+                <a href="{{ route('showFav')}}" class="btn btn-dark">♡</a>
             </div>
+
         </form>
-        <ul class="list-group">
-            @foreach($posts as $post)
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <a href="{{ route('showPost', $post) }}" class="text-decoration-none">{{ $post->title }}</a>
-                    <span class="text-muted">{{ $post->created_at->format('d.m.Y H:i') }}</span>
-                </li>
-            @endforeach
-        </ul>
-        @if($posts->count() == 0)
-            <p class="text-muted">Nothing found</p>
-        @endif
+
+            <ul class="list-group">
+                @foreach($posts as $post)
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <a href="{{ route('showPost', $post) }}" class="text-decoration-none">{{ $post->title }}</a>
+                        <span class="text-muted">{{ $post->created_at->format('d.m.Y H:i') }}</span>
+                    </li>
+                @endforeach
+            </ul>
+            @if($posts->count() == 0)
+                <p class="text-muted">Ничего не найдено</p>
+           @endif
     </div>
+
 @endsection
