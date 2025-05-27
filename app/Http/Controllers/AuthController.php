b070b558a5ebdfd403e31bcb56a900a813692072
shlendyk <shlendikgghggg@gmail.com>
2025-05-28 00:52:24 +0300

    AuthController refactoring

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
