3831d0ae66c99126f0a73cfea11d031778028af2
shlendyk <shlendikgghggg@gmail.com>
2025-05-28 20:47:56 +0300

    Add factory and seeders

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -2,17 +2,17 @@
 
 namespace Database\Seeders;
 
-use App\Models\Post;
+use App\Models\Comment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class PostSeeder extends Seeder
+class CommentSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Post::factory(10)->create();
+        Comment::factory(10)->create();
     }
 }
